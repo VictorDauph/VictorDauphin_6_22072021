0041b4191f829487b50dff5e19fdbea158b6d225
VictorDauph <victor.dauph@gmail.com>
2021-08-17 18:01:31 +0200

    ajout documentation multer-config

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -13,9 +13,9 @@ const storage = multer.diskStorage({
         callback(null, 'images') //images est le dossier de destination
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension );
+        const name = file.originalname.split(' ').join('_'); //suppression des espaces
+        const extension = MIME_TYPES[file.mimetype]; //définition des formats de fichiers supportés dans l'objet MIME_TYPES
+        callback(null, name + Date.now() + '.' + extension ); //formatage du nom de fichier: nom du fichier d'origine + timestamp + . + extension
     }
 });
 
